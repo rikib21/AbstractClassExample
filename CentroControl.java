a4aa96c5651b579315aaf82a392718a6151ed776
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-04-18 15:14:50 -0300

    Comments CentroControl.java

--- CentroControl.java
+++ CentroControl.java
@@ -1,3 +1,10 @@
+/*
+* Esta clase es utilizada para el control de todos
+* los vehiculos, posee una lista que almacena cualquier
+* tipo de vehiculo, almacena a traves de un metodo que lo agrega
+* a la lista primeramente,y posteriormente posee un metodo que 
+* monitorea todos los vehiculos.
+*/
 import java.util.ArrayList;
 
 public class CentroControl {
