95f8c81d4c603d549711af5f64f7278522ea5988
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-04-18 15:22:15 -0300

    Comments CamionAutonomo.java

--- CamionAutonomo.java
+++ CamionAutonomo.java
@@ -1,3 +1,11 @@
+/*
+*La clase representa un camion autonomo, que hereda
+* de vehiculo y tambien implementa la interfaz
+* Iconectable, implementa el movimiento de un camion
+* e implementa el metodo de interfaz que permite
+* sincronizar el GPS
+*/
+
 package POO.ABS;
 
 public class CamionAutonomo extends Vehiculo implements IConectable {
