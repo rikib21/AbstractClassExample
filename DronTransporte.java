07ad2bc191ec540ad4a158951a5860a53ab6a586
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-04-18 15:03:34 -0300

    Comments DronTransporte.java

--- DronTransporte.java
+++ DronTransporte.java
@@ -1,5 +1,8 @@
 
-
+/*
+* Clase que hereda de la clase vehiculo, represe-
+* ta un dron de transporte
+*/
 public class DronTransporte extends Vehiculo {
     @Override
     String patronMovimiento() {
