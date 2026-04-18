d87bdd8258f2db68accad36358d280182f5fb4f0
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-04-18 14:40:45 -0300

    Comments  Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,3 +1,10 @@
+/**
+ * Clase base abstracta que centraliza la identidad de cualquier vehículo.
+ *  Define un ID privado con su respectivo acceso (getter)
+ *  y establece un contrato obligatorio mediante un 
+ * método abstracto para que cada tipo de vehículo tenga su propio 
+ * patrón de movimiento.
+ */
 
 abstract class Vehiculo {
     private int id;
