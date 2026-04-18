9734f06ed5709a8b9873d1ada3c52f8264054eb5
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-04-18 14:53:36 -0300

    Comments IConectable.java

--- IConectable.java
+++ IConectable.java
@@ -1,5 +1,8 @@
 
-
+/*Interfaz que define la capacidad de conectarse a GPS,
+* y un metodo abstracto que se implementa para sincroni-
+* zar GPS
+*/
 interface IConectable{
     abstract void sincronizarGPS();
 }
