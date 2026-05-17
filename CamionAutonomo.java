36318343fa1258a4cb27c45f03ee5a3360ec9b15
fabianchaparro-rgb <fabian.chaparro@fiuni.edu.py>
2026-05-16 21:11:08 -0300

    Update CamionAutonomo.java

--- CamionAutonomo.java
+++ CamionAutonomo.java
@@ -21,5 +21,67 @@ public class CamionAutonomo extends Vehiculo implements IConectable {
     }   
     @Override 
     public List<CamionAutonomo> crearVehiculo(CamionAutonomo v){
+        camionAutonomo.add(v);
+        return camionAutonomo;
+    }
+    @Override
+    public void listarVehiculo(){
+        if(camionAutonomo.isEmpty()){
+            System.out.println("No hay camiones de transporte");
+        }
+        camionAutonomo.forEach(System.out::println);
+    }
+    @Override
+    public CamionAutonomom buscarVehiculo(String id){
+        return camionAutonomo.stream();
+        .filter(camion-> camion.getId().equals(id))
+        .findFirst()
+        .orseElse(null);
+    }
+    @Override
+    public CamionAutonomo modificarVehiculo(String id, String nuevoId){
+        CamionAutonomo existeCamion= buscarVehiculo(id);
+         Optional.ofNullable(existeDron)
+        .ifPresent(camion-> camion.setId(nuevoId));
+        return existeCamion;
+    }
+
+    @Override List<CamionAutonomo> eliminarVehiculo(String id){
+        CamionAutonomo camion = buscarVehiculo(id);
+        Optional.ofNullable(camion)
+        .ifPresent(camionT-> camionAutonomo.remove(camionT.getId()));
+        return dronTransporte;
+    }
+    public void filtrarVehiculos(String inicial){
+        camionAutonomo.stream()
+        .filter(v -> v.getId().startsWith(inicial))
+        .forEach(System.out::println);
+    }
+    public List<String> obtenerListaId(){
+        return camionAutonomo.stream()
+            .map(CamionAutonomo::getId)
+            .toList();
+    }
+    public List<DronTransporte> buscarVehiculoPorTipo(String tipo){
+        return camionAutonomo.stream()
+        .filter(v-> v.patronMovimiento().equalsIgnoreCase(tipo))
+        .toList();
+    }
+    public void ordenarVehiculosPorId(){
+        dronTransporte.stream()
+        .sorted(Comparator.comparing(CamionAutonomo::getId))
+        .forEach(System.out::println)
+    }
+
+    public void ordenarVehiculosPorTipo(){
+        camionAutonomo.stream()
+        .sorted(Comparator.comparing(CamionAutonomo::patronMovimiento))
+        .forEach(System.out::println)
+            
+    }
+}
+    
+    
+    
         
 }
